8214989271fb870d43eb1cfc860b329c4fa229bf
gustav0s0usa <gustarafa2005@gmail.com>
2021-06-14 19:25:19 -0300

    Adicionamos a tela de categorias e configuramos o botão que leva a ela no menu principal

---
+++ app/src/main/java/com/example/catalogame/Lancamentos.java
@@ -0,0 +1,15 @@
+package com.example.catalogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lancamentos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_lancamentos);
+    }
+}
