db411376b6d678d72470b82dc4adf01c4caa5524
gustav0s0usa <gustarafa2005@gmail.com>
2021-06-15 18:25:17 -0300

    Configuração dos botões e remoção da action bar na tela do jogo1

--- app/src/main/java/com/example/catalogame/Minecraft.java
+++ app/src/main/java/com/example/catalogame/Minecraft.java
@@ -2,13 +2,56 @@ package com.example.catalogame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Minecraft extends AppCompatActivity {
+
+    ImageButton botaoCate;
+    ImageButton botaoHom;
+    ImageButton botaoLanc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minecraft);
+
+        getSupportActionBar().hide();
+
+        botaoCate = findViewById(R.id.btncatmine);
+        botaoHom = findViewById(R.id.btnhomemine);
+        botaoLanc = findViewById(R.id.btnlancmine);
+
+        botaoCate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent cate = new Intent(getApplicationContext(), Categorias.class);
+                startActivity(cate);
+            }
+
+        });
+
+        botaoHom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent ho = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ho);
+            }
+
+        });
+
+        botaoLanc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent lanc = new Intent(getApplicationContext(), Lancamentos.class);
+                startActivity(lanc);
+            }
+
+        });
     }
 }
