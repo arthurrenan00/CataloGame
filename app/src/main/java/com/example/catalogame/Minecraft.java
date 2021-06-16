8952481ee7732ba3b02c195c67fa7bcf21520708
gustav0s0usa <gustarafa2005@gmail.com>
2021-06-16 12:14:47 -0300

    Adição das reviews dos jogos 1 e 2

--- app/src/main/java/com/example/catalogame/Minecraft.java
+++ app/src/main/java/com/example/catalogame/Minecraft.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ImageButton;
 
 public class Minecraft extends AppCompatActivity {
@@ -17,12 +18,21 @@ public class Minecraft extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minecraft);
-
         getSupportActionBar().hide();
 
         botaoCate = findViewById(R.id.btncatmine);
         botaoHom = findViewById(R.id.btnhomemine);
         botaoLanc = findViewById(R.id.btnlancmine);
+
+        WebView mWebView = (WebView) findViewById(R.id.webviewRm);
+
+        String text = "<html><style type=\"text/css\">p{text-align:justify}</style><body>"
+                + "<p>"
+                + getString(R.string.reviewMine)
+                + "</p> "
+                + "</body></html>";
+
+        mWebView.loadData(text, "text/html", "utf-8");
 
         botaoCate.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/catalogame/Village.java
+++ app/src/main/java/com/example/catalogame/Village.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ImageButton;
 
 public class Village extends AppCompatActivity {
@@ -22,6 +23,16 @@ public class Village extends AppCompatActivity {
         botaoCate = findViewById(R.id.btncatvil);
         botaoHom = findViewById(R.id.btnhomevil);
         botaoLanc = findViewById(R.id.btnlancvil);
+
+        WebView mWebView = (WebView) findViewById(R.id.webviewRm1);
+
+        String text = "<html><style type=\"text/css\">p{text-align:justify}</style><body>"
+                + "<p>"
+                + getString(R.string.reviewvil)
+                + "</p> "
+                + "</body></html>";
+
+        mWebView.loadData(text, "text/html", "utf-8");
 
         botaoCate.setOnClickListener(new View.OnClickListener() {
             @Override
