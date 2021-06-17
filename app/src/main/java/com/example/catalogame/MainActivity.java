a0f48637e2377a0f878745a89382c7fdf9185fa8
gustav0s0usa <gustarafa2005@gmail.com>
2021-06-17 11:26:48 -0300

    Correção do botão steam

--- app/src/main/java/com/example/catalogame/MainActivity.java
+++ app/src/main/java/com/example/catalogame/MainActivity.java
@@ -7,12 +7,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import java.io.UnsupportedEncodingException;import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton botaoCategorias;
     ImageButton botaoLanc;
     ImageButton botaoMine;
+    ImageButton botaoSteam;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         botaoCategorias = findViewById(R.id.btncat1);
         botaoLanc = findViewById(R.id.btnlanc1);
         botaoMine = findViewById(R.id.btnmine1);
+        botaoSteam = findViewById(R.id.btnsteam);
 
         botaoCategorias.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,14 +54,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(mine);
             }
         });
+
+        botaoSteam.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://store.steampowered.com/?l=portuguese");
+                Intent it = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(Intent.createChooser(it, getString(R.string.chNav)));
+
+            }
+        });
     }
-    
-    public void abrirSteam(View view)
-    {
-	Uri uri = Uri.parse("https://store.steampowered.com/?l=portuguese");
-	Intent it = new Intent(Intent.ACTION_VIEW,uri);
-	startActivity(Intent.createChooser(it, getString(R.string.chNav)));
-    }
-    
+
 
 }
