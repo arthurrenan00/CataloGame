dfb4225d0f740badd1f9f8d793229e869fb84fbe
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2021-06-22 18:11:24 -0300

    Programando as funções dos ImageButtons

--- app/src/main/java/com/example/catalogame/CatSandbox.java
+++ app/src/main/java/com/example/catalogame/CatSandbox.java
@@ -6,9 +6,59 @@ import android.os.Bundle;
 
 public class CatSandbox extends AppCompatActivity {
 
+    ImageButton botaoCate;
+    ImageButton botaoHom;
+    ImageButton botaoLanc;
+    ImageButton botaoMine;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_sandbox);
+        getSupportActionBar().hide();
+
+        botaoCate = findViewById(R.id.btncatterror);
+        botaoHom = findViewById(R.id.btnhometerror);
+        botaoLanc = findViewById(R.id.btnlancterror);
+        botaoMine = findViewById(R.id.btnmine2);
+        
+        botaoCate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent cate = new Intent(getApplicationContext(), Categorias.class);
+                startActivity(cate);
+            }
+
+        });
+
+        botaoHom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent ho = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ho);
+            }
+
+        });
+
+        botaoLanc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent lanc = new Intent(getApplicationContext(), Lancamentos.class);
+                startActivity(lanc);
+            }
+
+        });
+
+        botaoMine.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent mine = new Intent(getApplicationContext(), Minecraft.class);
+                startActivity(mine);
+            }
+        });
     }
 }
