d8bd946fab56eb5e2eaf2ca315144983e28ee477
gustav0s0usa <gustarafa2005@gmail.com>
2021-06-22 18:06:22 -0300

    Adição das categorias sandbox e fps

---
+++ app/src/main/java/com/example/catalogame/CatSandbox.java
@@ -0,0 +1,14 @@
+package com.example.catalogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatSandbox extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_sandbox);
+    }
+}
