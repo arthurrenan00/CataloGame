1becf57d23325e923748141c8097a1b23258a9c7
gustav0s0usa <gustarafa2005@gmail.com>
2021-06-22 17:57:02 -0300

    configuração da tela terror

--- app/src/main/java/com/example/catalogame/CatTerror.java
+++ app/src/main/java/com/example/catalogame/CatTerror.java
@@ -2,13 +2,67 @@ package com.example.catalogame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CatTerror extends AppCompatActivity {
+
+    ImageButton botaoCate;
+    ImageButton botaoHom;
+    ImageButton botaoLanc;
+    ImageButton botaoRevil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_terror);
+        getSupportActionBar().hide();
+
+        botaoCate = findViewById(R.id.btncatterror);
+        botaoHom = findViewById(R.id.btnhometerror);
+        botaoLanc = findViewById(R.id.btnlancterror);
+        botaoRevil = findViewById(R.id.btnrevil2);
+
+
+        botaoCate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent cate = new Intent(getApplicationContext(), Categorias.class);
+                startActivity(cate);
+            }
+
+        });
+
+        botaoHom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent ho = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ho);
+            }
+
+        });
+
+        botaoLanc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent lanc = new Intent(getApplicationContext(), Lancamentos.class);
+                startActivity(lanc);
+            }
+
+        });
+
+        botaoRevil.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent revil = new Intent(getApplicationContext(), Village.class);
+                startActivity(revil);
+            }
+        });
     }
 }
