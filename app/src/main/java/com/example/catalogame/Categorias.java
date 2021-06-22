ea3d028109f481eb9d2c310ea91883bbef2cb435
gustav0s0usa <gustarafa2005@gmail.com>
2021-06-22 17:33:52 -0300

    Adição da tela terror

--- app/src/main/java/com/example/catalogame/Categorias.java
+++ app/src/main/java/com/example/catalogame/Categorias.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Categorias extends AppCompatActivity {
 
     ImageButton botaoHome;
     ImageButton botaoLanca;
+    Button botaofps;
+    Button botaosandbox;
+    Button botaoterror;
 
     
     @Override
