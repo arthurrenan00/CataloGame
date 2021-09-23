bb36cb3aabc145939336e6d8b69fb3d1fc03e0e5
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2021-09-23 13:14:46 -0300

    Tirando a parte de acesso de localização
    Tiramos esta parte para colocar em outra activity

--- app/src/main/java/com/example/catalogame/MainActivity.java
+++ app/src/main/java/com/example/catalogame/MainActivity.java
@@ -24,27 +24,7 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity  implements EnderecoAtual.OnTaskCompleted {
-
-    public static final String PREFERENCIAS_NAME = "com.example.android.localizacao";
-    private static final String TRACKING_LOCATION_KEY = "tracking_location";
-    private static final int REQUEST_LOCATION_PERMISSION = 1;
-    private static final String LATITUDE_KEY = "latitude";
-    private static final String LONGITUDE_KEY = "longitude";
-    private static final String LASTDATE_KEY = "data";
-
-    private Button mLocationButton;
-    private TextView mLocationTextView;
-    private static final String LASTADRESS_KEY = "adress";
-    private FusedLocationProviderClient mFusedLocationClient;
-    private LocationCallback mLocationCallback;
-
-    private boolean mTrackingLocation;
-
-    private SharedPreferences mPreferences;
-    private String lastLatitude = "";
-    private String lastLongitude = "";
-    private String lastAdress = "";
+public class MainActivity extends AppCompatActivity {
 
     ImageButton botaoCategorias;
     ImageButton botaoLanc;
@@ -62,48 +42,6 @@ public class MainActivity extends AppCompatActivity  implements EnderecoAtual.On
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide(); // remove a action bar
         setContentView(R.layout.activity_main);
-
-        
-        
-        mLocationButton = (Button) findViewById(R.id.btnlocalizacao);
-        mLocationTextView = (TextView) findViewById(R.id.txtlocalizacao);
-// Inicializa FusedLocationClient.
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(
-                this);
-// Recupera o estado da aplicação quando é recriado
-        if (savedInstanceState != null) {
-            mTrackingLocation = savedInstanceState.getBoolean(
-                    TRACKING_LOCATION_KEY);
-        }
-
-// Listener do botão de localização.
-        mLocationButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (!mTrackingLocation) {
-                    startTrackingLocation();
-                } else {
-                    stopTrackingLocation();
-                }
-            }
-        });
-
-        // Inicializa os callbacks da locations.
-        mLocationCallback = new LocationCallback() {
-
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                // If tracking is turned on, reverse geocode into an address
-                if (mTrackingLocation) {
-                    new EnderecoAtual(MainActivity.this, MainActivity.this)
-                            .execute(locationResult.getLastLocation());
-                }
-            }
-        };
-        mPreferences = getSharedPreferences(PREFERENCIAS_NAME, MODE_PRIVATE);
-        recuperar();
-
 
         botaoCategorias = findViewById(R.id.btncat1);
         botaoLanc = findViewById(R.id.btnlanc1);
@@ -202,132 +140,6 @@ public class MainActivity extends AppCompatActivity  implements EnderecoAtual.On
             }
             
         });
-    }
-
-                         
-    
-    //
-
-    private void startTrackingLocation() {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]
-                            {Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_LOCATION_PERMISSION);
-        } else {
-            mTrackingLocation = true;
-            mFusedLocationClient.requestLocationUpdates
-                    (getLocationRequest(),
-                            mLocationCallback,
-                            null /* Looper */);
-            // Set a loading text while you wait for the address to be
-            // returned
-            mLocationTextView.setText(getString(R.string.endereco,
-                    getString(R.string.loading), null, null,
-                    System.currentTimeMillis()));
-            mLocationButton.setText(R.string.parar_busca);
-        }
-    }
-
-    private LocationRequest getLocationRequest() {
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        return locationRequest;
-    }
-
-
-    private void stopTrackingLocation() {
-        if (mTrackingLocation) {
-            mTrackingLocation = false;
-            mLocationButton.setText(R.string.iniciar_busca);
-            mLocationTextView.setText(R.string.hint);
-        }
-    }
-
-    /**
-     * SAVED INSTANCE NO APP
-     */
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putBoolean(TRACKING_LOCATION_KEY, mTrackingLocation);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_LOCATION_PERMISSION:
-                // Permissão garantida
-                if (grantResults.length > 0
-                        && grantResults[0]
-                        == PackageManager.PERMISSION_GRANTED) {
-                    startTrackingLocation();
-                } else {
-                    Toast.makeText(this,
-                            R.string.permissao_recusada,
-                            Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
-    }
-
-
-
-    @Override
-    public void onTaskCompleted(String[] result) {
-        if (mTrackingLocation) {
-            // Update the UI
-            lastLatitude = result[1];
-            lastLongitude = result[2];
-            lastAdress = result[0];
-            mLocationTextView.setText(getString(R.string.endereco,
-                    lastAdress, lastLatitude, lastLongitude, System.currentTimeMillis()));
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mTrackingLocation) {
-            stopTrackingLocation();
-            mTrackingLocation = true;
-            armazenar(lastLatitude, lastLongitude, lastAdress);
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        if (mTrackingLocation) {
-            startTrackingLocation();
-        }
-        recuperar();
-        super.onResume();
-    }
-
-
-    private void armazenar(String latitude, String longitude, String lastAdress) {
-        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
-        preferencesEditor.putString(LATITUDE_KEY, latitude);
-        preferencesEditor.putString(LONGITUDE_KEY, longitude);
-        preferencesEditor.putLong(LASTDATE_KEY, System.currentTimeMillis());
-        preferencesEditor.putString(LASTADRESS_KEY, lastAdress);
-        preferencesEditor.apply();
-    }
-
-    private void recuperar() {
-        lastLatitude = mPreferences.getString(LATITUDE_KEY, "");
-        lastLongitude = mPreferences.getString(LONGITUDE_KEY, "");
-        long time = mPreferences.getLong(LASTDATE_KEY, 0);
-        lastAdress = mPreferences.getString(LASTADRESS_KEY, "");
-        Toast.makeText(this,
-                getString(R.string.endereco,
-                        lastAdress, lastLatitude, lastLongitude, time),
-                Toast.LENGTH_SHORT).show();
-
     }
 
 
